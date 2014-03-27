bdcd35251bd1d0558cbd8305b6e9626e943d46b7
aaronroe <aaronalkan@gmail.com>
2014-03-27 13:01:41 -0500

    Adds rudimentary support for storing gifs using mongodb fs

--- vid2gifweb.py
+++ vid2gifweb.py
@@ -2,12 +2,15 @@ import os
 from flask import Flask, request
 from werkzeug.utils import secure_filename
 from moviepy.editor import *
+from flask.ext.pymongo import PyMongo
 
 UPLOAD_FOLDER = 'file-uploads'
 ALLOWED_EXTENSIONS = set(['mp4'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+mongo = PyMongo(app)
 
 def allowed_file(filename):
   return '.' in filename and \
@@ -17,18 +20,38 @@ def allowed_file(filename):
 def hello():
   return "Hello World!"
 
+@app.route("/uploads")
+def view_uploads():
+	rows = ""
+	for i in range(mongo.db.fs.files.find().count()):
+		rows += str(mongo.db.fs.files.find()[i])
+	return rows
+
 @app.route("/upload", methods=['POST'])
 def upload_video():
 	file = request.files['video']
-	print file
+	
 	if file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
 		name_with_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 		file.save(name_with_path)
 		
-		VideoFileClip(name_with_path).subclip((0,0.0),(0,4.0)).resize(0.3).to_gif("converted/"+filename.rsplit('.', 1)[0]+".gif")
+		gifname = filename.rsplit('.', 1)[0]+".gif"
+		gifpath = "converted/"+gifname
 
+		# create the 0-5 second gif at 0.3 of the size
+		VideoFileClip(name_with_path).subclip((0,0.0),(0,5.0)).resize(0.3).to_gif("converted/"+gifname)
+		
+		# save the converted gif to mongofs
+		giffile = open(gifpath)
+		mongo.save_file(gifname, giffile)
+		giffile.close()
+
+		# remove temporarily stored files
 		os.remove(name_with_path)
+		os.remove(gifpath)
+
+		print mongo.db.fs.files.find().count()
 
 	return ""
 
